4b30f7a3cd9d592786b5bb831ae7afbb5a91866c
raxmatjon <raxmatjonhamidov242@gmail.com>
2023-08-15 23:55:03 +0500

    to merge first commit of branch main

--- restaurants/models.py
+++ restaurants/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from products.models import Product
 
 # Create your models here.
-# class Restaurants(models.Model):
-#     name=models.CharField(max_length=60)
-#     product=models.ForeignKey(Product,related_name='offers')
-#     location=models.CharField(max_length=100, blank=True, null=True)
+class Restaurants(models.Model):
+    restaurant_picture=models.ImageField(default='default_restoran_profile')
+    name=models.CharField(max_length=60)
+    product=models.ForeignKey(Product,related_name='offers')
+    location=models.CharField(max_length=100, blank=True, null=True)
